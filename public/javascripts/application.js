ef188c859e14503f8545fc1afa3863c4e7977ecf
westonludeke <westonludeke@gmail.com>
2021-08-02 15:52:39 -0500

    added jquery code to confirm destructive actions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,13 @@
+$(function() {
 
+  $('form.delete').submit(function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      var ok = confirm("Are you sure? This cannot be undone!");
+      if (ok) {
+        this.submit();
+      }
+  });
+
+});
