53f3acfe3e856536ce77903d72ce00a120d9f823
westonludeke <westonludeke@gmail.com>
2021-08-18 10:56:42 -0500

    updated application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,26 @@
 $(function() {
 
   $("form.delete").submit(function(event) {
-      event.preventDefault();
-      event.stopPropagation();
+    event.preventDefault();
+    event.stopPropagation();
 
-      var ok = confirm("Are you sure? This cannot be undone!");
-      if (ok) {
-        var form = $(this);
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      var form = $(this);
 
-        var request = $.ajax({
-          url: form.attr("action"),
-          method: form.attr("method")
-        });
+      var request = $.ajax({
+        url: form.attr("action"),
+        method: form.attr("method")
+      });
 
-        request.done(function(data, TextStatus, jqXHR) {
-          if (jqXHR.status === 204) {
-            form.parent("li").remove();
-          } else if (jqXHR.status === 200) {
-            document.location = data;
-          }
-        });
-      }
+      request.done(function(data, textStatus, jqXHR) {
+        if (jqXHR.status == 204) {
+          form.parent("li").remove();
+        } else if (jqXHR.status == 200) {
+          document.location = data;
+        }
+      });
+    }
   });
 
 });
